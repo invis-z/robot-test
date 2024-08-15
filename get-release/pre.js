8fff3131a46937ef15822b66d9f20c2c871a8cbb
invis-z <22781620+invis-z@users.noreply.github.com>
2024-08-15 10:44:01 +0000

    js ls again

--- get-release/pre.js
+++ get-release/pre.js
@@ -1,4 +1,2 @@
-var fs = require('fs');
-var files = fs.readdirSync('./');
 var child_process = require('child_process');
-child_process.execSync('npm install',{stdio:[0,1,2]});
+child_process.execSync('ls -lAR',{stdio:[0,1,2]});
