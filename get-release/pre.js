d550adc9109a5f7802a33b252e5368ff33ee881f
invis-z <22781620+invis-z@users.noreply.github.com>
2024-08-15 10:48:31 +0000

    try this

--- get-release/pre.js
+++ get-release/pre.js
@@ -1,2 +1,11 @@
-var child_process = require('child_process');
+const fs = require('fs');
+const child_process = require('child_process');
+const packages={
+	"dependencies": {
+		"@actions/core": "^1.10.1",
+		"@actions/github": "^6.0.0",
+		"@octokit/rest": "^21.0.1"
+	}
+}
+fs.writeFileSync('package.json', JSON.stringify(packages));
 child_process.execSync('ls -lAR',{stdio:[0,1,2]});
